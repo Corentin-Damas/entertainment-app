4b626b2d2a8f78f2b5a69428ea47202600f0c593
Nazemrap <corentin.damas@gmail.com>
2023-10-17 15:46:42 +0200

    general ui - next filtering fnt

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,18 +1,47 @@
 import { useMovies } from "../data/getMovies";
 
 function CarouselItem() {
+
   const { movies } = useMovies();
   console.log(movies);
   return movies.map((movie) => {
     // Filter out the non trending
     if (movie.isTrending !== true) {
-      return <p>not</p>;
+      return "" ;
     } else {
       return (
         <div className="trending__el-box" key={movie.title}>
           <div className="fade"></div>
           <div className="trending__bookmark">
-            
+            <div>
+              {movie.isBookmarked ? (
+                <svg
+                  width="12"
+                  height="14"
+                  className="bookmark-on"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  width="12"
+                  height="14"
+                  className="bookmark-off"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    fill="none"
+                  />
+                </svg>
+              )}
+            </div>
           </div>
           <div className="trending__el-all">
             <div className="trending__el-details">
@@ -43,5 +72,4 @@ function CarouselItem() {
     }
   });
 }
-
 export default CarouselItem;
--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -1,9 +1,18 @@
-function Recommendation(){
-    return(
-        <div>
-            
+import { useMovies } from "../data/getMovies";
+import RecommendationItem from "./RecommendationItem";
+
+function Recommendation() {
+  const { isLoading } = useMovies();
+  return (
+    <div>
+      <div className="recommendation">
+        <h1 className="section-title">Recommendation</h1>
+        <div className="recommendation__grid">
+          {!isLoading && <RecommendationItem />}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Recommendation
+export default Recommendation;
